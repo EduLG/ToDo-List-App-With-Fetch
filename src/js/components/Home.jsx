d60c65ba87a59eec5f029b325a8c984a16f897e1
Eduardo Ladron de Guevara <eduladron@gmail.com>
2025-05-07 19:29:44 +0000

    made the to do list wothout fetching

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,78 @@
 import React from "react";
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [newTask,setNewTask] = useState("");
+	const [tasks,setTasks] = useState([]);
+	const [completedTasks, setCompletedTasks] = useState([]);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	const handleAddTask = (event) => {
+		if (event.key === "Enter" && newTask.trim() !== "") {
+			setTasks([...tasks, newTask.trim()]);
+			setNewTask("");
+		}
+	};
+
+
+
+	const handleDoneTask = (indexToDone) => {
+		const doneTask = tasks[indexToDone]
+		setCompletedTasks([...completedTasks, doneTask.trim()]);
+		setTasks(tasks.filter((_, index) => index !== indexToDone));
+	}
+
+	return (
+		<>
+			<div className="text-center container">
+				<h1 className="text-center mt-5">To do list:</h1>
+				<input 
+					className="form-control"
+					placeholder="Insert a task to do..."
+					onChange={event => setNewTask(event.target.value)}
+					value={newTask}
+					onKeyDown={handleAddTask}
+				/>
+				<ul className=" list-group mt-3">
+					{tasks.length === 0 ? (
+						<li className="list-group-item justify-content-between align-items-center d-flex">No tasks. Please add a new task.</li>
+					):(
+						tasks.map((task,index) => (
+							<li key={index} className="list-group-item justify-content-between align-items-center d-flex">
+								<span>{task}</span>
+								<div>
+
+									<button
+										className="btn btn-success btn-sm inv-btn"
+										onClick={() => handleDoneTask(index)}
+									>
+										<i className="fa-solid fa-check"></i>
+									</button>
+									
+								</div>
+							</li>
+						))
+					)}
+				</ul>
+			</div>
+
+			<div>
+				<p className="text-center">{tasks.length} tasks left</p>
+			</div>
+
+			<div className="text-center container">
+			<h2 className="text-center mt-5">Done tasks:</h2>
+				<ul className=" list-group mt-3">
+					{completedTasks.map((completedTask, index) => (
+						<li key={index} className="list-group-item justify-content-between align-items-center d-flex">
+							<span>{completedTask}</span>
+						</li>
+					))
+					}
+				</ul>
+			</div>
+
+		</>
 	);
 };
 
